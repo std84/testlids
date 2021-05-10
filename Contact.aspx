5f6f6bced372f5d0d4f16f9129c48cd5cc6ad9cf
std84 <vizen84@gmail.com>
2021-05-10 23:14:11 +0300

    Add files via upload

---
+++ Contact.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="lidstest.Contact" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+   
+    <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
+    <asp:GridView CssClass="Grid" runat="server" ID="myGV" AutoGenerateColumns="False" AlternatingRowStyle-CssClass="alt"
+        onselectedindexchanged="CustomersGridView_SelectedIndexChanged"
+        AutoGenerateSelectButton="True" 
+        Width="305px" OnRowDataBound="myGV_RowDataBound">
+        <Columns>
+            <asp:BoundField ControlStyle-Width="200px" HeaderText="Name" DataField="Name" />
+            <asp:BoundField ControlStyle-Width="200px" HeaderText="amount" DataField="amount" />
+
+        </Columns>
+        
+    </asp:GridView>
+
+        <asp:UpdatePanel ID="UpdatePanel1"
+            runat="server">
+            
+            <ContentTemplate>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
+                        <div class="form-group">
+                            <label for="name" class="control-label"> name</label>
+                            <asp:TextBox ID="name" placeholder="name" runat="server"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="RequiredFieldname" runat="server" 
+                                Style="top: 98px; left: 367px; position: absolute; height: 26px; width: 162px" 
+                                ErrorMessage="name required" ControlToValidate="name">
+                            </asp:RequiredFieldValidator>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
+                        <div class="form-group">
+                            <label for="amount" class="control-label">keywords</label>
+                            <asp:TextBox ID="amount" placeholder="amount" runat="server"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="RequiredFieldamount" runat="server" 
+                                Style="top: 98px; left: 367px; position: absolute; height: 26px; width: 162px" 
+                                ErrorMessage="amount required" ControlToValidate="amount">
+                            </asp:RequiredFieldValidator>
+                                <asp:RangeValidator ID="amountRange" runat="server" Style="top: 194px; left: 365px;  
+        position: absolute; height: 22px; width: 105px" ErrorMessage="RangeValidator"  
+        ControlToValidate="amount" MaximumValue="999" MinimumValue="100" Type="Integer"></asp:RangeValidator> 
+                        </div>
+                    </div>
+                </div>
+        <asp:Button runat="server" Text="Button" OnClick="Unnamed1_Click" />
+            </ContentTemplate>
+        </asp:UpdatePanel>
+
+</asp:Content>
---
+++ Contact.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lidstest
+{
+    public partial class Contact : Page
+    {
+        DataTable dt = new DataTable();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            initData();
+        }
+        private void initData()
+        {
+            dt.Clear();
+            dt.Columns.Add("Name");
+            dt.Columns.Add("amount");
+            dt.Columns.Add("sum");
+            DataRow _ravi = dt.NewRow();
+            _ravi["Name"] = "ravi";
+            _ravi["amount"] = "500";
+            dt.Rows.Add(_ravi);
+            _ravi = dt.NewRow();
+            _ravi["Name"] = "eddi";
+            _ravi["amount"] = "250";
+            dt.Rows.Add(_ravi);
+            _ravi = dt.NewRow();
+            _ravi["Name"] = "asd";
+            _ravi["amount"] = "100";
+            dt.Rows.Add(_ravi);
+            _ravi = dt.NewRow();
+            _ravi["Name"] = "nn";
+            _ravi["amount"] = "700";
+            dt.Rows.Add(_ravi);
+            _ravi = dt.NewRow();
+            _ravi["Name"] = "ss";
+            _ravi["amount"] = "300";
+            dt.Rows.Add(_ravi);
+            _ravi = dt.NewRow();
+            _ravi["Name"] = "vv";
+            _ravi["amount"] = "400";
+            dt.Rows.Add(_ravi);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            myGV.DataSource = dt;
+            myGV.DataBind();
+            
+        }
+        protected void GridView2_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+               
+           
+                e.Row.BackColor = Color.Cyan;
+                string str = "";
+                str = "aa";
+                // e.Row.Cells[2].Text = Decrypt(e.Row.Cells[2].Text);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            for(int i = 0; i < dt.Rows.Count; i++)
+            {
+                sum = sum + int.Parse(dt.Rows[i]["amount"].ToString());
+            
+            }
+            DataRow _ravi = dt.NewRow();
+            _ravi["sum"] = sum;
+            dt.Rows.Add(_ravi);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        public void CustomersGridView_SelectedIndexChanged(Object sender, EventArgs e)
+        {
+            // Get the currently selected row using the SelectedRow property.
+            GridViewRow row = GridView1.SelectedRow;
+            GridView rowtest = (GridView)sender;
+            GridViewRow tester = rowtest.SelectedRow;
+            // Display the first name from the selected row.
+            // In this example, the third column (index 2) contains
+            // the first name.
+            name.Text= tester.Cells[1].Text;
+            amount.Text = tester.Cells[2].Text;
+        //    string s =  "You selected " + row.Cells[2].Text + ".";
+            string str = "";
+            str = "aa";
+        }
+        protected void myGV_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                if (Double.Parse(e.Row.Cells[2].Text, System.Globalization.NumberStyles.Currency) >= 10000.00)
+                {
+                    e.Row.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+
+                if (Double.Parse(e.Row.Cells[2].Text) >= 500)
+                {
+                    e.Row.ForeColor = System.Drawing.Color.Red;
+                    e.Row.Cells[1].Text = "expensive";
+                }
+                e.Row.BackColor = Color.Cyan;
+                string str = "";
+                str = "aa";
+                // e.Row.Cells[2].Text = Decrypt(e.Row.Cells[2].Text);
+            }
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            string str;
+            str = "22";
+        }
+    }
+}
